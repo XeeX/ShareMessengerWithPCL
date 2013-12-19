95c8d92cf516873a9dcd173e5e3889629904da3e
Kévin Rapaille <kevin.rapaille@hotmail.com>
2013-12-19 14:34:57 +0100

    Add a private member for the messenger in the PCL. Thanks @frederikprijck for the suggestion

--- ShareMessengerWithPCL.PCL/HelloWorldService.cs
+++ ShareMessengerWithPCL.PCL/HelloWorldService.cs
@@ -3,16 +3,25 @@ using ShareMessengerWithPCL.PCL.Interface;
 
 namespace ShareMessengerWithPCL.PCL
 {
+    /*
+     *  The sample was orignilay created for a blog post presenting the OverrideDeault method.
+     *  This is why I'll let the usage of this one in comment but in real life
+     *  you should use a private member like _messenger here.
+     */
+
     public class HelloWorldService : IHelloWorldService
     {
+        private readonly IMessenger _messenger;
         public HelloWorldService(IMessenger messenger)
         {
-            Messenger.OverrideDefault(messenger);
+            //Messenger.OverrideDefault(messenger);
+            _messenger = messenger;
         }
 
         public void SayHello()
         {
-            Messenger.Default.Send("Hello World !");
+            //Messenger.Default.Send("Hello World !");
+            _messenger.Send("Hello World !");
         }
     }
 }
